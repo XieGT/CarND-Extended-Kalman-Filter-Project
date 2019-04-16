070ae2a2a65d4ffc6637c47196548ca05f12ca54
Xie Zheng <xiezheng@wireless-10-104-39-228.umd.edu>
2019-04-16 12:13:36 -0400

    ready to submit

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -36,8 +36,9 @@ FusionEKF::FusionEKF() {
    * TODO: Finish initializing the FusionEKF.
    * TODO: Set the process and measurement noises
    */
-
-
+  
+  H_laser_ << 1, 0, 0, 0,
+             0, 1, 0, 0;
 }
 
 /**
@@ -61,15 +62,46 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.x_ = VectorXd(4);
     ekf_.x_ << 1, 1, 1, 1;
 
+    // setup state covariance matrix
+    ekf_.P_ = MatrixXd(4,4);
+    ekf_.P_ << 1, 0, 0, 0,
+               0, 1, 0, 0,
+               0, 0, 1000, 0,
+               0, 0, 0, 1000;
+    
+    // setup transition matrix
+    ekf_.F_ = MatrixXd(4,4);
+    ekf_.F_ << 1, 0, 1, 0,
+               0, 1, 0, 1,
+               0, 0, 1, 0,
+               0, 0, 0, 1;
+
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       // TODO: Convert radar from polar to cartesian coordinates 
       //         and initialize state.
 
+      double rho = measurement_pack.raw_measurements_[0];
+      double phi = measurement_pack.raw_measurements_[1];
+      double rhodot = measurement_pack.raw_measurements_[2];
+
+      double px = rho * cos(phi);
+      double py = rho * sin(phi);
+      double vx = rhodot * cos(phi);
+      double vy = rhodot * sin(phi);
+
+      ekf_.x_ << px, py, vx, vy;
+
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       // TODO: Initialize state.
+      double px = measurement_pack.raw_measurements_[0];
+      double py = measurement_pack.raw_measurements_[1];
+
+      ekf_.x_ << px, py, 0.0, 0.0;
 
     }
+
+    previous_timestamp_ = measurement_pack.timestamp_;
 
     // done initializing, no need to predict or update
     is_initialized_ = true;
@@ -87,6 +119,28 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
 
+  // compute time elapsed between the current and previous measurements
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0; //dt - expressed in seconds
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+  
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+  // modify the F matrix so that the time is integrated
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+
+  double noise_ax = 9.0;
+  double noise_ay = 9.0;
+
+  // set the process covariance matrix Q
+  ekf_.Q_ = MatrixXd(4,4);
+  ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
+          0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
+          dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+          0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
+
   ekf_.Predict();
 
   /**
@@ -101,9 +155,16 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_;
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
 
   } else {
     // TODO: Laser updates
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
 
   }
 
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -26,16 +26,62 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+  
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd S = H_ * P_ * H_.transpose() + R_;
+  MatrixXd K = P_ * H_.transpose() * S.inverse();
+
+  // new state
+  MatrixXd I = MatrixXd::Identity(4,4);
+  x_ = x_ + (K * y);
+  P_ = (I - K * H_) * P_;
+
+
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  // recover state parameters
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+  // --- convert to polar coordinates ---
+  double rho2 = px * px + py * py;
+  // for the very unlikely case we have an ant having fun with our lidar...
+  if(rho2 < 0.00001){
+    px = 0.0001;
+    py = 0.0001;
+    rho2 = sqrt(px*px + py*py);
+  }
+  double rho = sqrt(rho2);
+  double phi = atan2(py, px);
+  double rhodot = (px * vx + py * vy) / rho;
+
+  VectorXd z_pred(3);
+  z_pred << rho, phi, rhodot;
+  VectorXd y = z - z_pred;
+  // normalize the angel to [-180, 180]
+  y(1) = atan2(sin(y(1)), cos(y(1)));
+
+  MatrixXd S = H_ * P_ * H_.transpose() + R_;
+  MatrixXd K = P_ * H_.transpose() * S.inverse();
+
+  // new state
+  MatrixXd I = MatrixXd::Identity(4,4);
+  x_ = x_ + (K * y);
+  P_ = (I - K * H_) * P_;
+
+
 }
